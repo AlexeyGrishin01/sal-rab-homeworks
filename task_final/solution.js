cef83547e308b47108b9edb4ea3690d910ac93cb
Alexey Grishin <alabama_blues@mail.ru>
2022-07-14 13:28:45 +0500

    task final not done 2nd time

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,25 +1,24 @@
 function sendRequest(name, phone, address, goods, sum) {                          
                                                                                  
-  let data = {data: {client: "", goods: [], order: {}}};   
-    
-    data.client =  `${name} " " ${phone}`;
-    data.order = {address: `${address}`, sum: sum};
+    let data = {
+        data: {
+            client: "",
+            goods: [],
+            order: {}
+        }   
+    };
+   
+    data.client =  `${name} ${phone}`;
+  
+    data.order = {address: `${address.street, address.house, address.entrance, address.floor, address.flat}`, sum: sum};
 
     let countOfGoods = goods.length;                          
                                                                    
-    for (let i = 0; i < countOfGoods; i += 1) {                        
-        data.goods = [
-            {
-                title: goods[i].title,
-                count: goods[i].count
-            }
-        ];
-
-        data.goods.push(goods[i].title, goods[i].count);                           
-        
-    };                                                                    
+        for (let i = 0; i < countOfGoods; i += 1) {                        
+            data.goods.push({title: goods[i].title, count: goods[i].count});                           
+        };                                                                    
     
-    let jsonData = JSON.stringify({data});                   
+    let jsonData = JSON.stringify(data);                   
                                                            
     return jsonData;                                        
 }                                                        
